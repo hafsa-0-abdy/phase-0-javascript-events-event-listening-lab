a147ad983e3cd5ba3dce7618d0df3c71897d6070
Habsa Abdirizack <habsa.abdirizack@student.moringaschool.com>
2024-10-14 10:17:20 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-function addingEventListener() {
+ function addingEventListener() {
+        const input = document.getElementById('button');
+        input.addEventListener('click', clickAlert); 
+    }
+    addingEventListener(); 
+
+function clickAlert() {
+    alert('I was clicked!');
 }
+
+function addingEventListener() {
+    console.log("addingEventListener called");
+    const input = document.getElementById('button');
+    input.addEventListener('click', clickAlert);
+}
+
+    
+
